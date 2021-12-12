95312ce9bd9ff6fa24d0a23725701acc71e4fd9f
smigii <smigii@protonmail.com>
2021-12-12 17:10:54 -0600

    Connected query selector to query parameter pane

--- src/main/java/com/imqdb/Controller.java
+++ src/main/java/com/imqdb/Controller.java
@@ -1,7 +1,8 @@
 package com.imqdb;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.Tab;
@@ -9,21 +10,41 @@ import javafx.scene.control.Tab;
 public class Controller {
 	@FXML
 	private TabPane paramTabPane;
-
-	private int t = 1;
-
 	@FXML
-	protected void onHelloButtonClick()
+	private ChoiceBox<String> querySelector;
+
+	private SingleSelectionModel<Tab> queryPaneSelectionModel;
+
+	/*
+	Like a constructor, but called after everything is setup, put any component setup in here.
+	According to https://www.youtube.com/watch?v=9XJicRt_FaI 2:07:24, this should maybe be different but
+	fuck it this is cleaner.
+	*/
+	@FXML
+	public void initialize()
 	{
-		//welcomeText.setText("Welcome to JavaFX Application!");
+		// Tab Pane stuff
+		// Use this for setting active query pane
+		queryPaneSelectionModel = paramTabPane.getSelectionModel();
+
+		// Query selector
+		querySelector.setValue("Select a query!");
+		querySelector.getItems().add("Query 1");
+		querySelector.getItems().add("Query 2");
+
+		// Whenever selection changes, this method is called
+		querySelector.setOnAction(this::onQuerySelectionChanged);
+	}
+
+	public void onQuerySelectionChanged(ActionEvent event)
+	{
+		int queryIdx = querySelector.getSelectionModel().getSelectedIndex();
+		queryPaneSelectionModel.select(queryIdx);
 	}
 
 	@FXML
 	protected void onRunBtnClick()
 	{
 		System.out.println("TEST");
-		SingleSelectionModel<Tab> selectionModel = paramTabPane.getSelectionModel();
-		selectionModel.select(t);
-		t = (t+1)%2;
 	}
 }
