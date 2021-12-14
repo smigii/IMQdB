17e429eb4bca3868e83d2532e66491720dea2419
smigii <smigii@protonmail.com>
2021-12-14 15:54:10 -0600

    Quick test using parameters in queries

--- src/java/imqdb/QcNames.java
+++ src/java/imqdb/QcNames.java
@@ -1,5 +1,8 @@
 package imqdb;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Spinner;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,11 +10,15 @@ import java.sql.SQLException;
 
 public class QcNames implements QueryController {
 
+	@FXML
+	Spinner<Integer> limitSpinner;
+
 	@Override
 	public ResultSet execute(Connection db)
 	{
 		try {
-			PreparedStatement ps = db.prepareStatement("select name from actor limit 10");
+			int limit = limitSpinner.getValue();
+			PreparedStatement ps = db.prepareStatement("select name from actor order by imdb_name_id limit " + limit);
 			ResultSet rs = ps.executeQuery();
 			return rs;
 		}
