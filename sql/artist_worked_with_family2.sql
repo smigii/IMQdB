d1e1ce17bfb1212a614df7d6801ec2e671b19939
smigii <smigii@protonmail.com>
2021-12-22 00:48:54 -0600

    more progress on family query

--- sql/artist_worked_with_family2.sql
+++ sql/artist_worked_with_family2.sql
@@ -22,6 +22,33 @@ select m.original_title as "Movie", a1.name as "Artist", t1.title as Role, a2.na
 
 	where
 		tp.imdb_title_id = tp2.imdb_title_id
+	
+	union
+	
+	-- Artists worked with spouse
+	select
+		sa.imdb_name_id, tp.title_id, sa.spouse_id as family_id, tp2.title_id as family_role_id, tp.imdb_title_id, "Spouse" as "Relation"
+	from spouses_artist sa
+
+	inner join title_principals tp on tp.imdb_name_id = sa.imdb_name_id
+	inner join title_principals tp2 on tp2.imdb_name_id = sa.spouse_id
+
+	where
+		tp.imdb_title_id = tp2.imdb_title_id
+	
+	union
+	
+	-- Artists worked with relative
+	select
+		ra.imdb_name_id, tp.title_id, ra.relative_id as family_id, tp2.title_id as family_role_id, tp.imdb_title_id, "Relative" as "Relation"
+	from relatives_artist ra
+
+	inner join title_principals tp on tp.imdb_name_id = ra.imdb_name_id
+	inner join title_principals tp2 on tp2.imdb_name_id = ra.relative_id
+
+	where
+		tp.imdb_title_id = tp2.imdb_title_id
+		
 ) as x
 
 inner join artist a1 on x.imdb_name_id = a1.imdb_name_id
@@ -30,4 +57,5 @@ inner join titles t1 on x.title_id = t1.title_id
 inner join titles t2 on x.family_role_id = t2.title_id
 inner join movies m  on x.imdb_title_id = m.imdb_title_id
 
-where m.year = 2010;
+where
+	m.year = 2010
--- src/java/imqdb/qc/QcFamilyFun.java
+++ src/java/imqdb/qc/QcFamilyFun.java
@@ -13,37 +13,71 @@ public class QcFamilyFun implements QueryController {
 	@Override
 	public ResultSet execute(Connection db) throws SQLException
 	{
-		PreparedStatement ps = db.prepareStatement("select m.original_title as \"Movie\", a1.name as \"Artist\", t1.title as Role, a2.name as \"Family Member\", t2.title as \"Family Member Role\", Relation as \"Family Member Relation\" from (\n" +
-			"\t-- Artists worked with parent\n" +
-			"\tselect\n" +
-			"\t\tpa.imdb_name_id, tp.title_id, pa.parent_id as family_id, tp2.title_id as family_role_id, tp.imdb_title_id, \"Parent\" as \"Relation\"\n" +
-			"\tfrom parents_artist pa\n" +
-			"\n" +
-			"\tinner join title_principals tp on tp.imdb_name_id = pa.imdb_name_id\n" +
-			"\tinner join title_principals tp2 on tp2.imdb_name_id = pa.parent_id\n" +
-			"\n" +
-			"\twhere\n" +
-			"\t\ttp.imdb_title_id = tp2.imdb_title_id\n" +
-			"\t\n" +
-			"\tunion\n" +
-			"\t\n" +
-			"\t-- Artists worked with child\n" +
-			"\tselect\n" +
-			"\t\tca.imdb_name_id, tp.title_id, ca.child_imdb_id as family_id, tp2.title_id as family_role_id, tp.imdb_title_id, \"Child\" as \"Relation\"\n" +
-			"\tfrom children_artist ca\n" +
-			"\n" +
-			"\tinner join title_principals tp on tp.imdb_name_id = ca.imdb_name_id\n" +
-			"\tinner join title_principals tp2 on tp2.imdb_name_id = ca.child_imdb_id\n" +
-			"\n" +
-			"\twhere\n" +
-			"\t\ttp.imdb_title_id = tp2.imdb_title_id\n" +
-			") as x\n" +
-			"\n" +
-			"inner join artist a1 on x.imdb_name_id = a1.imdb_name_id\n" +
-			"inner join artist a2 on x.family_id = a2.imdb_name_id\n" +
-			"inner join titles t1 on x.title_id = t1.title_id\n" +
-			"inner join titles t2 on x.family_role_id = t2.title_id\n" +
-			"inner join movies m  on x.imdb_title_id = m.imdb_title_id\n"
+		PreparedStatement ps = db.prepareStatement(
+			"select m.original_title as \"Movie\", a1.name as \"Artist\", t1.title as Role, a2.name as \"Family Member\", t2.title as \"Family Member Role\", Relation as \"Family Member Relation\" from (\n" +
+				"\t-- Artists worked with parent\n" +
+				"\tselect\n" +
+				"\t\tpa.imdb_name_id, tp.title_id, pa.parent_id as family_id, tp2.title_id as family_role_id, tp.imdb_title_id, \"Parent\" as \"Relation\"\n" +
+				"\tfrom parents_artist pa\n" +
+				"\n" +
+				"\tinner join title_principals tp on tp.imdb_name_id = pa.imdb_name_id\n" +
+				"\tinner join title_principals tp2 on tp2.imdb_name_id = pa.parent_id\n" +
+				"\n" +
+				"\twhere\n" +
+				"\t\ttp.imdb_title_id = tp2.imdb_title_id\n" +
+				"\t\n" +
+				"\tunion\n" +
+				"\t\n" +
+				"\t-- Artists worked with child\n" +
+				"\tselect\n" +
+				"\t\tca.imdb_name_id, tp.title_id, ca.child_imdb_id as family_id, tp2.title_id as family_role_id, tp.imdb_title_id, \"Child\" as \"Relation\"\n" +
+				"\tfrom children_artist ca\n" +
+				"\n" +
+				"\tinner join title_principals tp on tp.imdb_name_id = ca.imdb_name_id\n" +
+				"\tinner join title_principals tp2 on tp2.imdb_name_id = ca.child_imdb_id\n" +
+				"\n" +
+				"\twhere\n" +
+				"\t\ttp.imdb_title_id = tp2.imdb_title_id\n" +
+				"\t\n" +
+				"\tunion\n" +
+				"\t\n" +
+				"\t-- Artists worked with spouse\n" +
+				"\tselect\n" +
+				"\t\tsa.imdb_name_id, tp.title_id, sa.spouse_id as family_id, tp2.title_id as family_role_id, tp.imdb_title_id, \"Spouse\" as \"Relation\"\n" +
+				"\tfrom spouses_artist sa\n" +
+				"\n" +
+				"\tinner join title_principals tp on tp.imdb_name_id = sa.imdb_name_id\n" +
+				"\tinner join title_principals tp2 on tp2.imdb_name_id = sa.spouse_id\n" +
+				"\n" +
+				"\twhere\n" +
+				"\t\ttp.imdb_title_id = tp2.imdb_title_id\n" +
+				"\t\n" +
+				"\tunion\n" +
+				"\t\n" +
+				"\t-- Artists worked with relative\n" +
+				"\tselect\n" +
+				"\t\tra.imdb_name_id, tp.title_id, ra.relative_id as family_id, tp2.title_id as family_role_id, tp.imdb_title_id, \"Relative\" as \"Relation\"\n" +
+				"\tfrom relatives_artist ra\n" +
+				"\n" +
+				"\tinner join title_principals tp on tp.imdb_name_id = ra.imdb_name_id\n" +
+				"\tinner join title_principals tp2 on tp2.imdb_name_id = ra.relative_id\n" +
+				"\n" +
+				"\twhere\n" +
+				"\t\ttp.imdb_title_id = tp2.imdb_title_id\n" +
+				"\t\t\n" +
+				") as x\n" +
+				"\n" +
+				"inner join artist a1 on x.imdb_name_id = a1.imdb_name_id\n" +
+				"inner join artist a2 on x.family_id = a2.imdb_name_id\n" +
+				"inner join titles t1 on x.title_id = t1.title_id\n" +
+				"inner join titles t2 on x.family_role_id = t2.title_id\n" +
+				"inner join movies m  on x.imdb_title_id = m.imdb_title_id\n" +
+				"\n" +
+				"where\n" +
+				"\tm.year = 2010\n" +
+				"\n" +
+				"order by\n" +
+				"\tm.imdb_title_id"
 			);
 		return ps.executeQuery();
 	}
